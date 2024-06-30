37f525173f0b0815ee9e2b61605239e85bea7065
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-06-30 11:52:21 +0600

    added stagger animation of framer motion

--- src/pages/Temp.tsx
+++ src/pages/Temp.tsx
@@ -1,14 +1,48 @@
 import { motion } from "framer-motion";
 
 const Temp = () => {
+  const parent = {
+    hidden: { opacity: 0, scale: 0.9 },
+    visible: { opacity: 1, scale: 1 },
+  };
+  const child = {
+    hidden: { opacity: 0, scale: 0.1 },
+    visible: { opacity: 1, scale: 1 },
+  };
   return (
     <div>
       <motion.div
-        className="size-40 bg-indigo-500 rounded-lg"
-        initial={{ rotate: 0 }}
-        animate={{ rotate: 360 }}
-        transition={{ type: "spring", duration: 2 }}
-      ></motion.div>
+        className="flex flex-wrap gap-5 p-5 justify-center items-center size-64 bg-indigo-500 rounded-lg"
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        transition={{
+          //   type: "spring",
+          //   repeat: Infinity,
+          //   repeatType: "reverse",
+          ease: "easeInOut",
+          duration: 2,
+          delayChildren: 1, // Renders children after a sec of rendering parent.
+          staggerChildren: 1, // Renders every child chronologically after 1 sec of previous child
+        }}
+      >
+        <motion.div
+          variants={child}
+          className="size-20 bg-cyan-500"
+        ></motion.div>
+        <motion.div
+          variants={child}
+          className="size-20 bg-cyan-500"
+        ></motion.div>
+        <motion.div
+          variants={child}
+          className="size-20 bg-cyan-500"
+        ></motion.div>
+        <motion.div
+          variants={child}
+          className="size-20 bg-cyan-500"
+        ></motion.div>
+      </motion.div>
     </div>
   );
 };
